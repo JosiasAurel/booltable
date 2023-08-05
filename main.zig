df91df16f3b4678f559aa476c3150aeb291cf77d
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-05 23:32:51 +0100

    initial commit

---
+++ main.zig
@@ -0,0 +1,65 @@
+// Booltable
+// A tool that takes in an expression such as A+B
+// and outputs a truth table
+// should also be able to compute expressions such as (A+B).C
+// and more complex ones (obviously)
+
+// How it works
+// 1. Identify the boolean variables
+// 2. Build an AST of the expression
+// 3. Evaluate the AST and give out the result
+// *4. Determine the steps required to go from bool var definition to final expression
+// *5. Print a tabular/any form of representation of that
+
+const std = @import("std");
+
+pub fn main() void {
+    simple_lexer("abcde");
+}
+
+const BoolDict = struct {
+    const Self = @This();
+    store: [100]u8 = undefined,
+    idx: usize = 0,
+
+    fn add(self: *Self, item: u8) void {
+        self.store[self.idx] = item;
+        self.idx += 1;
+    }
+
+    fn get(self: Self, idx: usize) u8 {
+        return self.store[idx];
+        // should throw an error
+        // if idx > self.idx
+    }
+
+    fn exists(self: Self, item: u8) bool {
+        for (self.store, 0..self.idx) |char, _| {
+            if (char == item) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// iterate over the expression string
+// return the unique boolean variables in the expression
+//
+fn simple_lexer(expression: []const u8) void {
+    var dict = BoolDict{};
+    for (expression) |char| {
+        if (is_alphanumeric(char) and !dict.exists(char)) {
+            dict.add(char);
+        }
+    }
+}
+
+// checks for whether a character is a letter (upper/lower-case)
+// checkout https://www.cs.cmu.edu/~pattis/15-1XX/common/handouts/ascii.html
+fn is_alphanumeric(char: u8) bool {
+    if ((char >= 65 and char <= 90) or (char >= 97 and char <= 122)) {
+        return true;
+    }
+    return false;
+}
