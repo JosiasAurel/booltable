79a65aaa94e7d29154d697aa73b12406a85331c6
JosiasAurel <ndjosiasaurel@gmail.com>
2023-08-06 12:00:34 +0100

    some progress ig?

--- main.zig
+++ main.zig
@@ -21,9 +21,15 @@ const std = @import("std");
 const print = std.debug.print;
 const BoundedArray = std.BoundedArray;
 const ConvBoundedArray = BoundedArray(u8, 40);
+const StringHashMap = std.StringHashMap;
+const ExpressionStore = StringHashMap(ConvBoundedArray);
+const HeapAllocator = std.heap.GeneralPurposeAllocator(.{});
+const allocator = HeapAllocator.allocator();
+
+const expression_store = ExpressionStore.init(allocator);
 
 pub fn main() !void {
-    const result = simple_lexer("abcde");
+    const result = simple_lexer("a+b");
     for (result.tokens()) |char| {
         print("Got token {c}\n", .{char});
     }
@@ -44,6 +50,22 @@ pub fn main() !void {
 
     var a_r7 = try adjust(8, r7.constSlice());
     print("7 = {s}\n", .{a_r7.constSlice()});
+
+    var ast = parse("a");
+    print_ast(ast);
+    ast = parse("a+b");
+    print_ast(ast);
+}
+
+fn print_ast(ast: AstNodeKind) void {
+    switch (ast) {
+        .uniq => print("uniq => {c}\n", .{ast.uniq}),
+        .node => {
+            print_ast(ast.node.left);
+            print("op -> {c}\n", .{ast.node.operator});
+            print_ast(ast.node.right);
+        },
+    }
 }
 
 const BoolDict = struct {
@@ -72,7 +94,7 @@ const BoolDict = struct {
     }
 
     fn tokens(self: Self) []const u8 {
-        return self.store[0 .. self.idx - 1];
+        return self.store[0..self.idx];
     }
 };
 
@@ -161,6 +183,89 @@ fn adjust(var_len: usize, str: []const u8) !ConvBoundedArray {
 
     try future_buffer.resize(idx);
     return future_buffer;
+}
+
+const TokenStream = struct {
+    const Self = @This();
+    tokens: []const u8,
+    idx: usize = 0,
+
+    fn consume(self: *Self) u8 {
+        const result = self.tokens[self.idx];
+        self.idx += 1;
+        return result;
+    }
+};
+
+// this step should involve constructing the table
+// of the boolean expression
+// so as we encounter an expression, we should build
+
+const AstNode = struct { left: AstNodeKind, operator: u8, right: AstNodeKind };
+const AstNodeKind = union(enum) { uniq: u8, node: *AstNode };
+
+fn parse(expression: []const u8) AstNodeKind {
+    if (expression.len == 1) return AstNodeKind{ .uniq = expression[0] };
+
+    var tokens = TokenStream{ .tokens = expression };
+
+    const left_node = tokens.consume();
+    const next_token = tokens.consume();
+    if (is_op(next_token)) {
+        const right_node = parse(expression[tokens.idx..expression.len]);
+
+        switch (right_node) {
+            .uniq => {
+                const buffer = [3]u8{ left_node, next_token, right_node.uniq };
+                _ = buffer;
+                // return AstNodeKind{ .node = &AstNode{ .left = AstNodeKind{ .uniq = left_node }, .operator = next_token, .right = @constCast(AstNodeKind{ .uniq = right_node.uniq }) } };
+            },
+
+            .node => {
+                // return AstNodeKind{ .node = &AstNode{ .left = AstNodeKind{ .node = left_node }, .operator = next_token, .right = AstNodeKind{ .node = right_node } } };
+            },
+        }
+
+        // ast node
+    }
+
+    return AstNodeKind{ .uniq = left_node };
+}
+
+fn gen_and_put_truth(str: [3]u8) void {
+    const x = expression_store.get([1]u8{str[0]}).constSlice();
+    const y = expression_store.get([1]u8{str[2]}).constSlice();
+    const buffer = ConvBoundedArray.init(20);
+    var idx: usize = 0;
+
+    switch (str[1]) {
+        '+' => {
+            for (x, 0..x.len) |b, i| {
+                try buffer.set(idx, b | y[i]);
+            }
+        },
+        '.' => {
+            for (x, 0..x.len) |b, i| {
+                try buffer.set(idx, b & y[i]);
+            }
+        },
+        '!' => {},
+        _ => {},
+    }
+
+    try buffer.resize(8);
+    expression_store.putNoClobber(str, buffer);
+}
+
+fn is_op(tok: u8) bool {
+    return switch (tok) {
+        '+', '!', '.' => true,
+        else => false,
+    };
+}
+
+fn char_to_num(char: u8) u32 {
+    return @intCast(char - 48);
 }
 
 // Evaluating these boolean operations
